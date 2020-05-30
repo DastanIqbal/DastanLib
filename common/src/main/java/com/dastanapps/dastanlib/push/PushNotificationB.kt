05ba11a7cc641598610a9a76f84103a0334bac85
Iqbal Ahmed <iqbal.ahmed@xische.com>
2020-05-30 19:49:41 +0400

    bump versions

--- common/src/main/java/com/dastanapps/dastanlib/push/PushNotificationB.kt
+++ common/src/main/java/com/dastanapps/dastanlib/push/PushNotificationB.kt
@@ -1,16 +1,14 @@
 package com.dastanapps.dastanlib.push
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by dastaniqbal on 12/02/2019.
  * 12/02/2019 10:05
  */
 data class PushNotificationB(
-        var title: String,
-        var content: String,
-        var banner_url: String,
-        var push_type: String
-) {
-    private val TAG = this::class.java.simpleName
-
-    constructor() : this("", "", "", "")
-}
+        @SerializedName("title") var title: String,
+        @SerializedName("content")var content: String,
+        @SerializedName("banner_url")var bannerUrl: String,
+        @SerializedName("push_type") var pushType: String
+)
